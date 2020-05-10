c9b080931e9eee0c77ce2ea72cc0824255a60ee5
juanpablocanas <juanpablocanaslozano@gmail.com>
2020-05-09 23:41:25 -0500

    Update

--- Ejercicio Ordenamiento Archivos/EjercicioOrganizarArchivosSelection/SortSelection.c
+++ Ejercicio Ordenamiento Archivos/EjercicioOrganizarArchivosSelection/SortSelection.c
@@ -4,43 +4,26 @@
 #define TAM 500000
 #define NOMBREAR "max.txt" //Cambio Nombre Archivo
 
-void swap(int* a, int* b) 
+void swap(int *xp, int *yp) 
 { 
-    int t = *a; 
-    *a = *b; 
-    *b = t; 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
 } 
   
-
-int partition (int arr[], int low, int high) 
+void selectionSort(int arr[], int n) 
 { 
-    int pivot = arr[high];    
-    int i = (low - 1);  
-    int j;
+   int i, j, min_idx; 
   
-    for (j = low; j <= high- 1; j++){ 
-    
-        
-        if (arr[j] < pivot) 
-        { 
-            i++;    
-            swap(&arr[i], &arr[j]); 
-        } 
-    } 
-    swap(&arr[i + 1], &arr[high]); 
-    return (i + 1); 
-} 
-  
-
-void quickSort(int arr[], int low, int high) 
-{ 
-    if (low < high) 
+    for (i = 0; i < n-1; i++) 
     { 
-        int pi = partition(arr, low, high); 
-        quickSort(arr, low, pi - 1); 
-        quickSort(arr, pi + 1, high); 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+        swap(&arr[min_idx], &arr[i]); 
     } 
-} 
+}
 
 
 
@@ -73,7 +56,7 @@ int main(){
     printf("Despues");
    clock_t begin = clock();
     
-      quickSort(array,0,n-1);
+      selectionSort(array,ctr);
     
     
 
