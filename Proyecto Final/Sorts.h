858139dacef6b484cae0fcbca63d6096e7d0864b
juanpablocanas <juanpablocanaslozano@gmail.com>
2020-05-17 00:50:57 -0500

    Cambios dia 3
    Hoy me concentre más en el aspecto de ordenamiento y archivos

--- Proyecto Final/Sorts.h
+++ Proyecto Final/Sorts.h
@@ -36,6 +36,22 @@ void quickSort(int arr[], int low, int high)
     } 
 } 
 
+void insertionSort(int arr[], int n) 
+{ 
+    int i, key, j; 
+    for (i = 1; i < n; i++) { 
+        key = arr[i]; 
+        j = i - 1; 
+  
+        while (j >= 0 && arr[j] > key) { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
+    } 
+} 
+
+
 
 
 void printArray(int arr[], int n) 
